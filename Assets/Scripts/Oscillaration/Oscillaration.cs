feab24a4f1ac2d62e07912b93ca8ffd89d3e7d8a
Am1d1v <dimadiman1adima@gmail.com>
2024-12-22 00:32:06 +0300

    Slowdown effect

---
+++ Assets/Scripts/Oscillaration/Oscillaration.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Oscillaration : MonoBehaviour
+{
+    [SerializeField] Transform objectPos;
+
+    float counter = 3;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Oscillarating();
+    }
+
+    void Oscillarating()
+    {
+
+        
+
+        
+    }
+}
---
+++ Assets/Scripts/Tower/Slowdown.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Slowdown : MonoBehaviour
+{
+    public float range = 1f;
+
+    public float slowdown = 0.5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
