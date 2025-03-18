fd4a4ee4912d7879a6bb0202185f5a6918be112d
Am1d1v <dimadiman1adima@gmail.com>
2025-03-18 17:31:38 +0300

    Obstacles generato

--- Assets/Scripts/Player/ObjectsSpawner.cs
+++ Assets/Scripts/Player/ObjectsSpawner.cs
@@ -8,32 +8,50 @@ public class ObjectsSpawner : MonoBehaviour
 
     public Transform spawnPoint;
 
+    public int amountOfObstacles;
 
     private void Start()
     {
-        Spawn();
+        //Spawn();
+        GenerateObstacles();
     }
 
-    private void Spawn()
-    {
-        for (int i = 1; i <= x; i++)
-        {
-            for (int j = 0; j < y; j++)
-            {
-                if(Random.value > 0.5)
-                {
-                    Instantiate(objectToSpawn, transform.position += new Vector3(0f, 0f, 1.5f), Quaternion.identity);
-                }
-                else
-                {
-                    transform.position += new Vector3(0f, 0f, 1.5f);
-                    j++;
-                }
+    //private void Spawn()
+    //{
+    //    for (int i = 1; i <= x; i++)
+    //    {
+    //        for (int j = 0; j < y; j++)
+    //        {
+    //            if(Random.value > 0.7)
+    //            {
+    //                Instantiate(objectToSpawn, transform.position += new Vector3(0f, 0f, 1.5f), Quaternion.identity);
+
+
+    //            }
+    //            else
+    //            {
+    //                spawnPoint.position += new Vector3(0f, 0f, 1.5f);
+    //                j++;
+    //            }
+
+
                 
+    //        }
+
+    //        spawnPoint.position = new Vector3(i * 1.5f, 0f, 0f);
+    //    }
+
+    //}
+
+    private void GenerateObstacles()
+    {
+        for (int i = 0; i < amountOfObstacles; i++)
+        {
+            if (i == 0)
+            {
+                Instantiate(objectToSpawn, transform.position += Vector3.zero, Quaternion.identity);
             }
-
-            spawnPoint.position = new Vector3(i * 1.5f, 0f, 0f);
+            Instantiate(objectToSpawn, transform.position += new Vector3(0f, 0f, 6f), Quaternion.identity);
         }
-
     }
 }
