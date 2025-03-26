9ad8e84128e889e4d3ea47a36bd01ef994db580f
Am1d1v <dimadiman1adima@gmail.com>
2025-03-26 17:53:02 +0300

    Move to point

--- Assets/Scripts/Player/Objects/MoveToPoint.cs
+++ Assets/Scripts/Player/Objects/MoveToPoint.cs
@@ -6,18 +6,43 @@ public class MoveToPoint : MonoBehaviour
 
     public float moveSpeed;
 
+    public float ObjectSpawnTimer;
+    private float ObjectSpawnTimerCounter;
+
     public Transform[] startPoints;
     public Transform[] endPoints;
+
+    public Transform selectedStartPoint;
+    public Transform selectedEndPoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ObjectSpawnTimerCounter = ObjectSpawnTimer;
+
+        SelectSpawnPoint();
+        SelectEndPoint();
+        SpawnObject();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SelectSpawnPoint()
+    {
+        selectedStartPoint = startPoints[Random.Range(0, startPoints.Length)];
+    }
+
+    void SelectEndPoint()
+    {
+        selectedEndPoint = endPoints[Random.Range(0, endPoints.Length)];
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(objectToMove, selectedStartPoint.position, Quaternion.identity);
     }
 }
