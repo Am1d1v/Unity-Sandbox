1df881b7650897ce23811d032d328bb6419997ca
Am1d1v <dimadiman1adima@gmail.com>
2025-03-26 17:46:01 +0300

    Move to point

--- Assets/Scripts/Player/Objects/MoveToPoint.cs
+++ Assets/Scripts/Player/Objects/MoveToPoint.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class MoveToPoint : MonoBehaviour
 {
+    public GameObject objectToMove;
+
+    public float moveSpeed;
+
+    public Transform[] startPoints;
+    public Transform[] endPoints;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
