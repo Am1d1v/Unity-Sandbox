aa1d27a0f1615f56e33c28ec079d27ad0f940e20
Am1d1v <dimadiman1adima@gmail.com>
2025-03-29 12:59:53 +0300

    Follow camera

--- Assets/Scripts/Camera/CameraRotation.cs
+++ Assets/Scripts/Camera/CameraRotation.cs
@@ -7,6 +7,12 @@ public class CameraRotation : MonoBehaviour
 
     public GameObject objectToFollow;
 
+    public int currentAngle;
+
+    public float targetRotation;
+
+    public float rotateSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,12 +24,37 @@ public class CameraRotation : MonoBehaviour
     {
         transform.position = objectToFollow.transform.position + new Vector3(0f, 3.6f, -6f);
 
-        if (Keyboard.current.qKey.wasPressedThisFrame)
+        //if (Keyboard.current.qKey.wasPressedThisFrame)
+        //{
+        //    mainCamera.transform.eulerAngles += new Vector3(0f, 30f, 0f);
+        //} else if (Keyboard.current.eKey.wasPressedThisFrame)
+        //{
+        //    mainCamera.transform.eulerAngles += new Vector3(0f, -30f, 0f);
+        //}
+
+        // Camera rotation
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            mainCamera.transform.eulerAngles += new Vector3(0f, 30f, 0f);
-        } else if (Keyboard.current.eKey.wasPressedThisFrame)
-        {
-            mainCamera.transform.eulerAngles += new Vector3(0f, -30f, 0f);
+            currentAngle++;
+
+            if (currentAngle >= 4)
+            {
+                currentAngle = 0;
+            }
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            currentAngle--;
+
+            if (currentAngle < 0)
+            {
+                currentAngle = 3;
+            }
+        }
+
+        targetRotation = (90f * currentAngle);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(45f, targetRotation, 0f), rotateSpeed * Time.deltaTime);
     }
 }
