9d156a7eaeb6acb94ff6f9c19aa6ca80ce91c73d
Am1d1v <dimadiman1adima@gmail.com>
2025-04-05 19:53:35 +0300

    Clamp Spawn

--- Assets/Scripts/Clamp/ClampSpawn.cs
+++ Assets/Scripts/Clamp/ClampSpawn.cs
@@ -4,17 +4,25 @@ public class ClampSpawn : MonoBehaviour
 {
     public GameObject objectToSpawn;
 
-    public float minSpawnPoint, maxSpawnPoint;
+    public Transform minSpawnPoint, maxSpawnPoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SelectSpawnPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 SelectSpawnPoint()
+    {
+        float xPos = Random.Range(minSpawnPoint.position.x, maxSpawnPoint.position.x);
+        float yPos = Random.Range(minSpawnPoint.position.z, maxSpawnPoint.position.z);
+
+        return new Vector3(xPos, 0.6f, yPos);
     }
 }
