1f8ed68d2c332cde32cf488bd0d9c2b8421bc098
Am1d1v <dimadiman1adima@gmail.com>
2025-04-10 17:11:52 +0300

    Enemy Waves Spawner

--- Assets/Scripts/Spawner Pool/WaveSpawner.cs
+++ Assets/Scripts/Spawner Pool/WaveSpawner.cs
@@ -1,16 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
+    public GameObject[] enemiesToSpawn;
+
+    public int amountToSpawn;
+
+    public List<GameObject> activeEnemies = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CreateEnemies();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateEnemies()
     {
-        
+        for(int i = 0; i < amountToSpawn; i++)
+        {
+            GameObject newEnemy = Instantiate(enemiesToSpawn[Random.Range(0, enemiesToSpawn.Length)], transform.position, transform.rotation);
+
+            activeEnemies.Add(newEnemy);
+        }
     }
 }
