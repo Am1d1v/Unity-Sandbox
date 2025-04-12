5a8514ae2d42061de0b0e5e18cc83ddfcd0c0b97
Am1d1v <dimadiman1adima@gmail.com>
2025-04-12 22:17:05 +0300

    Box Collider

--- Assets/Scripts/Colliders/BoxCollider.cs
+++ Assets/Scripts/Colliders/BoxCollider.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class BoxCollider : MonoBehaviour
 {
+    public Collider[] objectsInRange;
+
+    public LayerMask objectToAdd;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,16 @@ public class BoxCollider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetObjects();
+    }
+
+    void GetObjects()
+    {
+        objectsInRange = Physics.OverlapBox(transform.position, transform.localPosition, Quaternion.identity, objectToAdd);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
     }
 }
