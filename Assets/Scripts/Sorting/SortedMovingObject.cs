5346d597ca3b0e6894f5efa0068f733c90c6358c
Am1d1v <dimadiman1adima@gmail.com>
2025-04-21 16:22:46 +0300

    Sorting Path

--- Assets/Scripts/Sorting/SortedMovingObject.cs
+++ Assets/Scripts/Sorting/SortedMovingObject.cs
@@ -6,6 +6,10 @@ public class SortedMovingObject : MonoBehaviour
 
     public float moveSpeed;
 
+    public bool atTheMiddlePoint;
+
+    public Transform pointToMove;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +19,35 @@ public class SortedMovingObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!atTheMiddlePoint)
+        {
+            MoveToTheMiddle();
+        }
+    }
+
+    void MoveToTheMiddle()
+    {
         transform.position = Vector3.MoveTowards(transform.position, SortingPath.instance.middlePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, SortingPath.instance.middlePoint.position) < 0.1f)
+        {
+            atTheMiddlePoint = true;
+        }
+    }
+
+    void MoveToTheSpicificPoint(string objectColor)
+    {
+        switch (objectColor)
+        {
+            case "Pink":
+
+                    break;
+            case "Black":
+
+                break;
+            case "Blue":
+
+                break;
+        }
     }
 }
