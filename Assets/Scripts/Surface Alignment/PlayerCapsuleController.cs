b304ec3f1bdec59dcc910aeb4e67b04130cd8e5f
Am1d1v <dimadiman1adima@gmail.com>
2025-07-12 14:48:38 +0300

    Surface Alignment

--- Assets/Scripts/Surface Alignment/PlayerCapsuleController.cs
+++ Assets/Scripts/Surface Alignment/PlayerCapsuleController.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class PlayerCapsuleController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+
+    [SerializeField] Vector3 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,9 @@ public class PlayerCapsuleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.deltaTime;
     }
 }
