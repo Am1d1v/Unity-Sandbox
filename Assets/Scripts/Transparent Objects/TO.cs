04c981fa67ec6f04a193552ce2ac5ff4e21a336f
Am1d1v <dimadiman1adima@gmail.com>
2025-09-25 12:19:51 +0300

    Object transparent

--- Assets/Scripts/Transparent Objects/TO.cs
+++ Assets/Scripts/Transparent Objects/TO.cs
@@ -4,14 +4,29 @@ public class TO : MonoBehaviour
 {
     [SerializeField] float distance;
 
+    [SerializeField] LayerMask objectMask;
+
     // Update is called once per frame
     void Update()
     {
-        
+        CastRay();
     }
 
     void CastRay()
     {
+        RaycastHit[] raycasts = Physics.RaycastAll(transform.position, transform.forward, distance, objectMask);
 
+        foreach(RaycastHit raycasthit in raycasts)
+        {
+            Debug.Log(raycasthit.collider.name);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        Gizmos.DrawLine(transform.position, transform.forward);
     }
 }
