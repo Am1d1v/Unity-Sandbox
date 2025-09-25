526983a5a2d05aea32b85ed228f791e875576fda
Am1d1v <dimadiman1adima@gmail.com>
2025-09-25 12:10:36 +0300

    Object transparent

---
+++ Assets/Scripts/Transparent Objects/TO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TO : MonoBehaviour
+{
+    [SerializeField] float distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CastRay()
+    {
+
+    }
+}
