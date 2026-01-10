e543e9ce0c3023ed7cfc45d9d510799f8be643e3
Am1d1v <dimadiman1adima@gmail.com>
2026-01-10 14:10:54 +0300

    Animation Event. PC

--- Assets/Scripts/Animation Event/AEPC.cs
+++ Assets/Scripts/Animation Event/AEPC.cs
@@ -3,8 +3,28 @@ using UnityEngine;
 public class AEPC : MonoBehaviour
 {
     [Header("Elements")]
-    [SerializeField] CapsuleCollider capsuleCollider;
+    [SerializeField] CharacterController characterController;
 
     [Header("Settings")]
     [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Vector3 moveDirection;
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        moveDirection = transform.forward * Input.GetAxisRaw("Vertical");
+
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    void Rotate()
+    {
+
+    }
 }
