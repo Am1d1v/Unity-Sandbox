cd77bb239bef1cdb75060687fe3417c9492215e2
Am1d1v <dimadiman1adima@gmail.com>
2026-02-12 13:14:04 +0300

    Town Click Event

--- Assets/Scripts/Town Click/TownClick.cs
+++ Assets/Scripts/Town Click/TownClick.cs
@@ -3,11 +3,6 @@ using UnityEngine.EventSystems;
 
 public class TownClick : MonoBehaviour, IPointerClickHandler
 {
-    //private void OnMouseDown()
-    //{
-    //    Debug.Log("Town Clicked");
-    //}
-
     public void OnPointerClick(PointerEventData eventData)
     {
         if(eventData.clickCount == 2)
