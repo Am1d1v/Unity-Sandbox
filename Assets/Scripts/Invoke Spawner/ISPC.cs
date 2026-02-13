7483a82df0adf73fd61df1b403bb4398fbba1deb
Am1d1v <dimadiman1adima@gmail.com>
2026-02-13 15:15:23 +0300

    Invoke Spawner

--- Assets/Scripts/Invoke Spawner/ISPC.cs
+++ Assets/Scripts/Invoke Spawner/ISPC.cs
@@ -10,8 +10,22 @@ public class ISPC : MonoBehaviour
 
     private void Update()
     {
+        Move();
+
+        Rotate();
+    }
+
+    void Move()
+    {
         moveInput = new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
 
         characterController.Move(moveInput * moveSpeed * Time.deltaTime);
+    }
+    void Rotate()
+    {
+        float rotationInput = Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0f, rotationInput, 0f));
+
     }
 }
