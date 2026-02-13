d5e753f4bbea8f17ed0b04788a2d82ea778f8c21
Am1d1v <dimadiman1adima@gmail.com>
2026-02-13 15:11:46 +0300

    Invoke Spawner

--- Assets/Scripts/Invoke Spawner/ISPC.cs
+++ Assets/Scripts/Invoke Spawner/ISPC.cs
@@ -5,5 +5,13 @@ public class ISPC : MonoBehaviour
     [Header("Settings")]
     [SerializeField] float moveSpeed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] Vector3 moveInput;
     [SerializeField] CharacterController characterController;
+
+    private void Update()
+    {
+        moveInput = new Vector3(0f, 0f, Input.GetAxisRaw("Vertical"));
+
+        characterController.Move(moveInput * moveSpeed * Time.deltaTime);
+    }
 }
