47f83309ca4b13f5ef971516f16244f2cab7b49d
Am1d1v <dimadiman1adima@gmail.com>
2026-02-13 15:07:30 +0300

    Invoke Spawner

---
+++ Assets/Scripts/Invoke Spawner/ISPC.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ISPC : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] CharacterController characterController;
+}
