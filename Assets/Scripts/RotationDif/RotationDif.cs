6826602fc1238d10805a99ab500bbadef8b89141
Am1d1v <dimadiman1adima@gmail.com>
2026-02-14 23:02:52 +0300

    Rotation Difference

--- Assets/Scripts/RotationDif/RotationDif.cs
+++ Assets/Scripts/RotationDif/RotationDif.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class RotationDif : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] GameObject target;
+    [SerializeField] float difference;
+
+    private void Update()
+    {
+        CalculateDifference();
+    }
+
+    void CalculateDifference()
+    {
+        dif = transform.rotation.eulerAngles.y - target.transform.rotation.eulerAngles.y;
+    }
 }
