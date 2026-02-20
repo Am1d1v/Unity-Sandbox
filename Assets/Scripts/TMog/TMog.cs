7ad5a6099b88b32250884553f32417228a25709c
Am1d1v <dimadiman1adima@gmail.com>
2026-02-20 15:00:13 +0300

    TMog

--- Assets/Scripts/TMog/TMog.cs
+++ Assets/Scripts/TMog/TMog.cs
@@ -7,5 +7,20 @@ public class TMog : MonoBehaviour
     [SerializeField] Transform weaponHolder;
     [SerializeField] GameObject currentVisual;
     [SerializeField] GameObject selectedVisual;
-    
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            UpdateVisual();
+        }
+    }
+
+    void UpdateVisual()
+    {
+        weaponHolder.GetChild(0).gameObject.SetActive(false);
+
+        Instantiate(selectedVisual, weaponHolder.position + selectedVisual.transform.position, selectedVisual.transform.rotation, weaponHolder);
+    }
 }
