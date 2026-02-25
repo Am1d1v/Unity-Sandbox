c076dd5fbebb3a91a3a27f581e837512d5235bc1
Am1d1v <dimadiman1adima@gmail.com>
2026-02-25 16:27:36 +0300

    Lift

--- Assets/Scripts/Lift/Lift.cs
+++ Assets/Scripts/Lift/Lift.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class Lift : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] GameObject movingPlatform;
+    [SerializeField] int selectedLevel;
+    [SerializeField] GameObject[] levels;
+
+    private void Update()
+    {
+        
+    }
 }
