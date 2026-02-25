d8b7b32bb556577184c5196b08d57ac82e790e04
Am1d1v <dimadiman1adima@gmail.com>
2026-02-25 16:30:46 +0300

    Lift

--- Assets/Scripts/Lift/Lift.cs
+++ Assets/Scripts/Lift/Lift.cs
@@ -1,16 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class Lift : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] GameObject movingPlatform;
+    [SerializeField] GameObject selectedLevel;
     [SerializeField] GameObject[] levels;
-    [SerializeField] int selectedLevel;
+    [SerializeField] int selectedLevelIndex;
     [SerializeField] float moveSpeed;
     
 
     private void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.M) && selectedLevel != null)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        StartCoroutine(MoveCO());
+    }
+
+    IEnumerator MoveCO()
+    {
+
     }
 }
