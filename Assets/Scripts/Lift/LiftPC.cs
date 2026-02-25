4ef5397418186b5eba74e31289e5e2941552199f
Am1d1v <dimadiman1adima@gmail.com>
2026-02-25 21:08:11 +0300

    Lift

--- Assets/Scripts/Lift/LiftPC.cs
+++ Assets/Scripts/Lift/LiftPC.cs
@@ -5,4 +5,23 @@ public class LiftPC : MonoBehaviour
     [Header("Settings")]
     [SerializeField] float moveSpeed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float moveInput;
+    [SerializeField] float rotationInput;
+    [SerializeField] Rigidbody rb;
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+
+        float yAxis = rb.linearVelocity.y;
+
+        rb.linearVelocity = transform.forward * moveInput * moveSpeed;
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, yAxis, rb.linearVelocity.z);
+
+    }
 }
