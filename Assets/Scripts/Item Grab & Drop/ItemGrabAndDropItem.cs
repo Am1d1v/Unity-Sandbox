594f3cec960c2b96acd0480df56108935fc872f6
Am1d1v <dimadiman1adima@gmail.com>
2026-02-27 18:50:13 +0300

    Item Grab & Drop

--- Assets/Scripts/Item Grab & Drop/ItemGrabAndDropItem.cs
+++ Assets/Scripts/Item Grab & Drop/ItemGrabAndDropItem.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class ItemGrabAndDropItem : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] float rotationSpeed;
+
     private void Start()
     {
         SetRandomRotation();
@@ -14,5 +18,28 @@ public class ItemGrabAndDropItem : MonoBehaviour
         float randomZrotationValue = Random.Range(0f, 360f);
 
         transform.rotation = Quaternion.Euler(randomXrotationValue, randomYrotationValue, randomZrotationValue);
+    }
+
+    public void SetDefaultRotationCO()
+    {
+        StartCoroutine(SetDefaultRotation());
+    }
+
+    IEnumerator SetDefaultRotation()
+    {
+        float xValue = transform.rotation.eulerAngles.x;
+        float yValue = transform.rotation.eulerAngles.y;
+        float zValue = transform.rotation.eulerAngles.z;
+
+        while(transform.rotation.x != 0 && transform.rotation.y != 0 && transform.rotation.z != 0)
+        {
+            xValue = Mathf.MoveTowards(xValue, 0f, rotationSpeed * Time.deltaTime); 
+            yValue = Mathf.MoveTowards(yValue, 0f, rotationSpeed * Time.deltaTime); 
+            zValue = Mathf.MoveTowards(zValue, 0f, rotationSpeed * Time.deltaTime); 
+
+            transform.rotation = Quaternion.Euler(xValue, yValue, zValue);
+
+            yield return null;
+        }
     }
 }
