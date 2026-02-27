2cae34a8073d3a3cc5c68ac4fe51ab8a6b2d202c
Am1d1v <dimadiman1adima@gmail.com>
2026-02-27 18:44:55 +0300

    Item Grab & Drop

--- Assets/Scripts/Item Grab & Drop/ItemGrabAndDropItem.cs
+++ Assets/Scripts/Item Grab & Drop/ItemGrabAndDropItem.cs
@@ -4,6 +4,11 @@ public class ItemGrabAndDropItem : MonoBehaviour
 {
     private void Start()
     {
+        SetRandomRotation();
+    }
+
+    void SetRandomRotation()
+    {
         float randomXrotationValue = Random.Range(0f, 360f);
         float randomYrotationValue = Random.Range(0f, 360f);
         float randomZrotationValue = Random.Range(0f, 360f);
