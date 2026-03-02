9eb961d9d35287b86e6d17a49c12793d9b2751d3
Am1d1v <dimadiman1adima@gmail.com>
2026-03-02 16:35:54 +0300

    Grab Item

--- Assets/Scripts/Grab Item/GrabItem.cs
+++ Assets/Scripts/Grab Item/GrabItem.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class GrabItem : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float mass;
+
+    private void Start()
+    {
+        SetMass();
+    }
+
+    void SetMass()
+    {
+        mass = GetComponent<Rigidbody>().mass;
+    }
+
+    public float GetMass()
+    {
+        return mass;
+    }
 }
