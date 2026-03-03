c77f478ba33062a078409b5334e306ba2feeda5a
Am1d1v <dimadiman1adima@gmail.com>
2026-03-03 14:17:00 +0300

    Roll Ball

--- Assets/Scripts/Roll Ball/RollBallPC.cs
+++ Assets/Scripts/Roll Ball/RollBallPC.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class RollBallPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotatiobSpeed;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Vector3 moveInput;
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+
+    }
+
+    void Rotate()
+    {
+
+    }
 }
