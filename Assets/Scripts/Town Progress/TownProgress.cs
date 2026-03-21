9f2560d964193419ad8c477756664db760ea2d7f
Am1d1v <dimadiman1adima@gmail.com>
2026-03-21 19:28:15 +0300

    Town Progress

--- Assets/Scripts/Town Progress/TownProgress.cs
+++ Assets/Scripts/Town Progress/TownProgress.cs
@@ -7,8 +7,30 @@ public class TownProgress : MonoBehaviour
     [SerializeField] int currentBuildingLevel;
     [SerializeField] GameObject[] guildBuilding;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            BuildGuild();
+        }
+    }
+
     void TownLevelUp()
     {
         townLevel++;
+    }
+
+    void BuildGuild()
+    {
+        if (currentBuildingLevel > guildBuilding.Length - 1) return;
+
+        currentBuildingLevel++;
+
+        for(int i = 0; i < currentBuildingLevel; i++)
+        {
+            guildBuilding[i].SetActive(true);
+        }
+
+        TownLevelUp();
     }
 }
