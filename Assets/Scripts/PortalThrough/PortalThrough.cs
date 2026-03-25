4927739a9323ee220ff180032fd63227d771ff32
Am1d1v <dimadiman1adima@gmail.com>
2026-03-25 15:28:09 +0300

    Portal through

--- Assets/Scripts/PortalThrough/PortalThrough.cs
+++ Assets/Scripts/PortalThrough/PortalThrough.cs
@@ -5,20 +5,48 @@ public class PortalThrough : MonoBehaviour
     [Header("Settings")]
     [SerializeField] float portalPointerMoveSpeed;
     [SerializeField] float detectionRadius;
-    [SerializeField] bool isMoving;
+    [SerializeField] bool collidedWithObstacle;
+    [SerializeField] bool pointerCasted;
     [SerializeField] LayerMask obstacleMask;
     [SerializeField] GameObject portalPointer;
 
     private void Update()
-    {
-        MovePortalPointer();
+    {      
+        if (Input.GetMouseButtonDown(0))
+        {
+            pointerCasted = true;
+        }
+        else if (Input.GetMouseButtonDown(1) && pointerCasted)
+        {
+            ResetPortalPointer();
+        }
+
+        if (pointerCasted)
+        {
+            MovePortalPointer();
+        }
     }
 
     void MovePortalPointer()
     {
+        portalPointer.transform.position += transform.up * portalPointerMoveSpeed * Time.deltaTime;
+    }
+
+    void DetectObstacle()
+    {
         Collider[] colliders = Physics.OverlapSphere(portalPointer.transform.position, detectionRadius, obstacleMask);
 
-        Debug.Log(colliders.Length);
+        if(colliders.Length > 0)
+        {
+            collidedWithObstacle = true;
+        }
+    }
+
+    void ResetPortalPointer()
+    {
+        pointerCasted = false;
+
+        portalPointer.transform.position = Vector3.zero;
     }
 
     private void OnDrawGizmos()
