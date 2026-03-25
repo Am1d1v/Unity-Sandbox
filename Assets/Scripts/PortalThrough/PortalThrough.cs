e06b99a4b9db677b6207dae6895bef6e9c603ad3
Am1d1v <dimadiman1adima@gmail.com>
2026-03-25 15:19:12 +0300

    Portal through

--- Assets/Scripts/PortalThrough/PortalThrough.cs
+++ Assets/Scripts/PortalThrough/PortalThrough.cs
@@ -4,15 +4,19 @@ public class PortalThrough : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float portalPointerMoveSpeed;
+    [SerializeField] float detectionRadius;
+    [SerializeField] bool isMoving;
     [SerializeField] GameObject portalPointer;
 
     private void Update()
     {
-        
+        MovePortalPointer();
     }
 
     void MovePortalPointer()
     {
+        Collider[] colliders = Physics.OverlapSphere(portalPointer.transform.position, detectionRadius);
 
+        Debug.Log(colliders.Length);
     }
 }
