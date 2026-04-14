47e4e5292ea1857ce32ce5b75119fb554fb6b964
Am1d1v <dimadiman1adima@gmail.com>
2026-04-15 00:09:36 +0300

    Holding Object

--- Assets/Scripts/Holding Object/HoldingObject.cs
+++ Assets/Scripts/Holding Object/HoldingObject.cs
@@ -4,6 +4,7 @@ public class HoldingObject : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] Vector3 mousePosition;
+    [SerializeField] Ray ray;
     private void Update()
     {
         GetMousePosition();
@@ -11,13 +12,20 @@ public class HoldingObject : MonoBehaviour
 
     void GetMousePosition()
     {
-        mousePosition = Input.mousePosition;
+        Vector3 mousePos = Input.mousePosition;
+
+        mousePosition = mousePos;
+
+        if (Physics.Raycast(ray))
+        {
+            Debug.Log(ray);
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
 
-        //Gizmos.DrawRay(Camera.main.transform.position, );
+        Gizmos.DrawRay(ray);
     }
 }
