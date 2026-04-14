fd75b347a1d2025b2fc760346889ad55be28008a
Am1d1v <dimadiman1adima@gmail.com>
2026-04-14 23:57:25 +0300

    Holding Object

--- Assets/Scripts/Holding Object/HoldingObject.cs
+++ Assets/Scripts/Holding Object/HoldingObject.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class HoldingObject : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] Vector3 mousePosition;
+    private void Update()
+    {
+        GetMousePosition();
+    }
+
+    void GetMousePosition()
+    {
+        mousePosition = Input.mousePosition;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //Gizmos.DrawRay(Camera.main.transform.position, );
+    }
 }
