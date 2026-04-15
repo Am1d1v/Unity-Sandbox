86bbcb9ca9258d2ea4ca8364f99d02cdaec0d9c4
Am1d1v <dimadiman1adima@gmail.com>
2026-04-15 14:59:03 +0300

    Holding Object

--- Assets/Scripts/Holding Object/HOObject.cs
+++ Assets/Scripts/Holding Object/HOObject.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class HOObject : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] float yValueTrashold;
+
+    private void Update()
+    {
+        if(transform.position.y <= yValueTrashold)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnMouseDown()
     {
         HoldingObject.instance.SetItem(gameObject);
