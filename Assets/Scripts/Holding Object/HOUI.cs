f9f1b29b949c56d6f87fec5dcf13e2aa5a4ae38c
Am1d1v <dimadiman1adima@gmail.com>
2026-04-16 15:13:19 +0300

    Holding Object

--- Assets/Scripts/Holding Object/HOUI.cs
+++ Assets/Scripts/Holding Object/HOUI.cs
@@ -1,6 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class HOUI : MonoBehaviour
 {
-    
+    public static HOUI instance;
+
+    [Header("Current Item")]
+    [SerializeField] TMP_Text currentHoldingItem;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void SetCurrentItemToSpawnText(string itemName)
+    {
+        currentHoldingItem.text = itemName;
+    }
 }
