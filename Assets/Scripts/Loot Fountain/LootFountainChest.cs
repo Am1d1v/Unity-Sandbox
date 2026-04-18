4cddb8e7e819c8eace3ca81191eebf9d2e7644c4
Am1d1v <dimadiman1adima@gmail.com>
2026-04-18 14:00:33 +0300

    Loot Fountain

--- Assets/Scripts/Loot Fountain/LootFountainChest.cs
+++ Assets/Scripts/Loot Fountain/LootFountainChest.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class LootFountainChest : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] bool isOpened;
+
+    void OpenChest()
+    {
+        Debug.Log("Open");
+
+        isOpened = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if(isOpened == false)
+        {
+            OpenChest();
+        }
+    }
 }
