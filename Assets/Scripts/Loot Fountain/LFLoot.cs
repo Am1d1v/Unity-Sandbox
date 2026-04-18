e7c0c7760d83ea71a03c289dcfe220b4127023b4
Am1d1v <dimadiman1adima@gmail.com>
2026-04-18 14:23:01 +0300

    Loot Fountain

--- Assets/Scripts/Loot Fountain/LFLoot.cs
+++ Assets/Scripts/Loot Fountain/LFLoot.cs
@@ -4,14 +4,22 @@ public class LFLoot : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] LootSO LootSO;
+    [SerializeField] MeshRenderer lootVisual;
+    [SerializeField] float length;
 
     private void Start()
     {
-        
+        ConfigureLoot();
     }
 
     void ConfigureLoot()
     {
+        int selectedMaterialIndex = Random.Range(0, LootSO.Materials.Length);
 
+        lootVisual.material = LootSO.Materials[selectedMaterialIndex];
+
+        length = Random.Range(LootSO.MinLength, LootSO.MaxLength);
+
+        transform.localScale = new Vector3(length, transform.localScale.y, transform.localScale.z);
     }
 }
