f59deaf26d7f056205760593843f35279652e704
Am1d1v <dimadiman1adima@gmail.com>
2026-04-18 14:36:09 +0300

    Loot Fountain

--- Assets/Scripts/Loot Fountain/LootFountainChest.cs
+++ Assets/Scripts/Loot Fountain/LootFountainChest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LootFountainChest : MonoBehaviour
@@ -5,12 +6,24 @@ public class LootFountainChest : MonoBehaviour
     [Header("Settings")]
     [SerializeField] bool isOpened;
     [SerializeField] Transform lootSpawnPoint;
+    [SerializeField] int lootAmount;
+    [SerializeField] LFLoot lootPrefab;
 
     void OpenChest()
     {
+        SetLoot();
+
         Debug.Log("Open");
 
         isOpened = true;
+    }
+
+    void SetLoot()
+    {
+        for(int i = 0; i < lootAmount; i++)
+        {
+            Instantiate(lootPrefab, lootSpawnPoint.position, Quaternion.identity);
+        }
     }
 
     private void OnMouseDown()
