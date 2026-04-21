14a6ad40e18f4c7f7d3e567a29226e0029e8af06
Am1d1v <dimadiman1adima@gmail.com>
2026-04-21 16:01:19 +0300

    Move Point

--- Assets/Scripts/Move Point/MovePoint.cs
+++ Assets/Scripts/Move Point/MovePoint.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class MovePoint : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector3 movePosition;
+
+    [Header("Elements")]
+    [SerializeField] Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        MoveRay();
+    }
+
+    void MoveRay()
+    {      
+        RaycastHit hit;
+
+        Physics.Raycast(mainCamera.transform.position, );
+    }
 }
