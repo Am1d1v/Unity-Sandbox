5c93342299050a4af73cce4b9667e20fe7198dd0
Am1d1v <dimadiman1adima@gmail.com>
2026-04-22 15:32:51 +0300

    Obstacle Transform

--- Assets/Scripts/Obstacle Transform/OTObstacle.cs
+++ Assets/Scripts/Obstacle Transform/OTObstacle.cs
@@ -5,5 +5,10 @@ public class OTObstacle : MonoBehaviour
     [Header("Settings")]
     [SerializeField] float moveSpeed;
     [SerializeField] float cameraRotationTreshold;
+    [SerializeField] float currentCameraRotationValue;
 
+    private void Update()
+    {
+        currentCameraRotationValue = Mathf.Abs(OTCameraController.instance.YRotationValue);
+    }
 }
