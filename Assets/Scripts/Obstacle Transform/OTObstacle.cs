dfe4dfb32522e8020e1d252855d6bde96afdcf4d
Am1d1v <dimadiman1adima@gmail.com>
2026-04-22 15:30:59 +0300

    Obstacle Transform

---
+++ Assets/Scripts/Obstacle Transform/OTObstacle.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class OTObstacle : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float cameraRotationTreshold;
+
+}
