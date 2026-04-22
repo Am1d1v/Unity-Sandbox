06d60717bbab35b778ac15cf85467404a456d260
Am1d1v <dimadiman1adima@gmail.com>
2026-04-22 15:34:29 +0300

    Obstacle Transform

--- Assets/Scripts/Obstacle Transform/OTObstacle.cs
+++ Assets/Scripts/Obstacle Transform/OTObstacle.cs
@@ -6,9 +6,21 @@ public class OTObstacle : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] float cameraRotationTreshold;
     [SerializeField] float currentCameraRotationValue;
+    [SerializeField] ObstacleState obstacleState;
+
+    private void Start()
+    {
+        obstacleState = ObstacleState.OnGround;
+    }
 
     private void Update()
     {
         currentCameraRotationValue = Mathf.Abs(OTCameraController.instance.YRotationValue);
     }
+}
+
+public enum ObstacleState
+{
+    OnGround,
+    UnderGround
 }
