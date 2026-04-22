3a5c3ae28ea474d06851579fe27438938a7c4373
Am1d1v <dimadiman1adima@gmail.com>
2026-04-22 15:28:10 +0300

    Obstacle Transform

--- Assets/Scripts/Obstacle Transform/OTCameraController.cs
+++ Assets/Scripts/Obstacle Transform/OTCameraController.cs
@@ -2,16 +2,33 @@ using UnityEngine;
 
 public class OTCameraController : MonoBehaviour
 {
+    public static OTCameraController instance;
+
     [Header("Settings")]
     [SerializeField] float moveSpeed;
+    [SerializeField] float yRotationValue;
+    public float YRotationValue => yRotationValue;
 
     [Header("Elements")]
     [SerializeField] Camera mainCamera;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Update()
     {
         float rotationInput = Input.GetAxisRaw("Horizontal");
 
         transform.Rotate(new Vector3(0f, rotationInput * moveSpeed, 0f) * Time.deltaTime);
+
+        yRotationValue = transform.rotation.eulerAngles.y;
+
+        if (transform.rotation.eulerAngles.y > 180f)
+        {
+            yRotationValue -= 360f;
+        }
+
     }
 }
