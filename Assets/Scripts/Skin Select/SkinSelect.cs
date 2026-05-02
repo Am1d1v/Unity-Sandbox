50f4aded4b793884216aeca1d7d25bed4174ef59
Am1d1v <dimadiman1adima@gmail.com>
2026-05-02 16:59:24 +0300

    Select Skin

--- Assets/Scripts/Skin Select/SkinSelect.cs
+++ Assets/Scripts/Skin Select/SkinSelect.cs
@@ -2,8 +2,5 @@ using UnityEngine;
 
 public class SkinSelect : MonoBehaviour
 {
-    [Header("Settings")]
-    [SerializeField] GameObject torso;
-    [SerializeField] GameObject head;
-    [SerializeField] GameObject[] shoulders;    
+      
 }
