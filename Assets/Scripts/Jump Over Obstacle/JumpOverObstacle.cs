eddc3a5bd2af8a6d0cd097f1792c317fbf5e3863
Am1d1v <dimadiman1adima@gmail.com>
2026-05-22 12:36:17 +0300

    Jump Over Obstacle

--- Assets/Scripts/Jump Over Obstacle/JumpOverObstacle.cs
+++ Assets/Scripts/Jump Over Obstacle/JumpOverObstacle.cs
@@ -4,7 +4,17 @@ public class JumpOverObstacle : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float moveSpeed;
+    [SerializeField] float detectionRadius;
+    [SerializeField] LayerMask obstacleLayer;
+    [SerializeField] Vector3 datectionZone;
 
     [Header("Elements")]
     [SerializeField] Rigidbody rb;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position + datectionZone, detectionRadius);
+    }
 }
