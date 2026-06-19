8f337b5cbed7a0032116a57d04b6def0c8e17708
Am1d1v <dimadiman1adima@gmail.com>
2026-06-19 15:22:18 +0300

    Crane Rotation

--- Assets/Scripts/Crane Rotation/CraneRotation.cs
+++ Assets/Scripts/Crane Rotation/CraneRotation.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 public class CraneRotation : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] GameObject crane;
     [SerializeField] GameObject rotationTarget;
+
+    private void Update()
+    {
+        LookAtTarget();
+    }
+
+    void LookAtTarget()
+    {
+        transform.rotation = Quaternion.LookRotation(rotationTarget.transform.position);
+    }
 }
