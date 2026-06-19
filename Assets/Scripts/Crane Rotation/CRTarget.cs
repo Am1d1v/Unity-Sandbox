5480f8ae6eb3c970873dc914428af2eaa8948925
Am1d1v <dimadiman1adima@gmail.com>
2026-06-19 15:34:35 +0300

    Crane Rotation

--- Assets/Scripts/Crane Rotation/CRTarget.cs
+++ Assets/Scripts/Crane Rotation/CRTarget.cs
@@ -1,6 +1,12 @@
+using System;
 using UnityEngine;
 
 public class CRTarget : MonoBehaviour
 {
-    
+    public static event Action<GameObject> onSelectRotationTarget;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("A");
+    }
 }
