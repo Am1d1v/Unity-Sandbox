91b742ad660269574a167166fdd4023f93718231
Am1d1v <dimadiman1adima@gmail.com>
2026-06-24 14:42:09 +0300

    Call to Arms

--- Assets/Scripts/Call to Arms/CallToArms.cs
+++ Assets/Scripts/Call to Arms/CallToArms.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class CallToArms : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float callRange;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireSphere(transform.position, callRange);
+    }
 }
