cf9c1f66b9c30e70676ab6306c06155e71d90a9b
Am1d1v <dimadiman1adima@gmail.com>
2026-06-24 14:49:03 +0300

    Call to Arms

--- Assets/Scripts/Call to Arms/CallToArms.cs
+++ Assets/Scripts/Call to Arms/CallToArms.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CallToArms : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float callRange;
+    [SerializeField] List<GameObject> calledUnits = new List<GameObject>();
+    [SerializeField] CallToArmsEnemyType enemyType;
+    public CallToArmsEnemyType EnemyType => enemyType;
+
+    [ContextMenu("Call Units")]
+    void CallUnits()
+    {
+        Collider[] effectedUnits = Physics.OverlapSphere(transform.position, callRange);
+
+        foreach(Collider unit in effectedUnits)
+        {
+            if(unit.TryGetComponent<CallToArms>(out CallToArms enemy) && enemy.EnemyType == CallToArmsEnemyType.Warrior)
+            {
+                calledUnits.Add(unit.gameObject);
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
@@ -11,4 +29,10 @@ public class CallToArms : MonoBehaviour
 
         Gizmos.DrawWireSphere(transform.position, callRange);
     }
+}
+
+public enum CallToArmsEnemyType
+{
+    Caller,
+    Warrior
 }
