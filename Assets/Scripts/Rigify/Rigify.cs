54deaee430972ab2522a584a891fe0f2d83a0a42
Am1d1v <dimadiman1adima@gmail.com>
2026-07-19 16:45:49 +0300

    Rigify

--- Assets/Scripts/Rigify/Rigify.cs
+++ Assets/Scripts/Rigify/Rigify.cs
@@ -2,5 +2,19 @@ using UnityEngine;
 
 public class Rigify : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] Rigidbody[] RBs;
+
+    private void Start()
+    {
+        SetKinematic();
+    }
+
+    void SetKinematic()
+    {
+        foreach(Rigidbody rb in RBs)
+        {
+            rb.isKinematic = true;
+        }
+    }
 }
