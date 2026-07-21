7d7a96665e095411be4bba785aa09a8dc6d36817
Am1d1v <dimadiman1adima@gmail.com>
2026-07-21 22:17:56 +0300

    Followers

--- Assets/Scripts/Followers/Followers.cs
+++ Assets/Scripts/Followers/Followers.cs
@@ -6,7 +6,8 @@ public class Followers : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float moveSpeed;
-    [SerializeField] float stoppingDistance;   
+    [SerializeField] float stoppingDistance;
+    [SerializeField] Transform orderDestination;
 
     [Header("Elements")]
     [SerializeField] Transform player;
@@ -21,6 +22,23 @@ public class Followers : MonoBehaviour
 
     private void Update()
     {
-        navMeshAgent.SetDestination(player.position);
+        if(orderDestination == null)
+        {
+            navMeshAgent.SetDestination(orderDestination.position);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(player.position);
+        }
+
+        if(transform.position == orderDestination.position)
+        {
+            orderDestination = null;
+        }
+    }
+
+    public void SetOrderPosition(Transform orderPosition)
+    {
+        orderDestination = orderPosition;
     }
 }
