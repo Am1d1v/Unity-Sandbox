dedcabff9e1f7c8a50d20a3c1844a30c732e0ac9
Am1d1v <dimadiman1adima@gmail.com>
2026-07-21 20:44:50 +0300

    Followers

--- Assets/Scripts/Followers/Followers.cs
+++ Assets/Scripts/Followers/Followers.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Followers : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+
+    [Header("Elements")]
+    [SerializeField] Transform player;
+    [SerializeField] NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent.speed = moveSpeed;
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(player.position);
+    }
 }
