a2680406f870c13541efc83a8e2c98351960e66e
Am1d1v <dimadiman1adima@gmail.com>
2026-07-23 12:55:22 +0300

    Wall Run

--- Assets/Scripts/Wall Run/WallRunPC.cs
+++ Assets/Scripts/Wall Run/WallRunPC.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WallRunPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpStrength;
+    [SerializeField] Vector3 moveDirection;
+
+    [Header("Elements")]
+    [SerializeField] Rigidbody rb;
+
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference moveInputAction;
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
 }
