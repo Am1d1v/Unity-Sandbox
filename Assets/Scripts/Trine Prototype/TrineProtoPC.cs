ef6c4c26e22b1429236a57ba7cc2f9186226fc52
Am1d1v <dimadiman1adima@gmail.com>
2026-07-28 15:06:33 +0300

    Trine Prototype

--- Assets/Scripts/Trine Prototype/TrineProtoPC.cs
+++ Assets/Scripts/Trine Prototype/TrineProtoPC.cs
@@ -4,10 +4,16 @@ public class TrineProtoPC : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] Vector3 mousePos;
+    [SerializeField] Vector3 raycastHit;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float rayLength;
+
 
     private void Update()
     {
         GetMousePos();
+
+        GetRaycast();
     }
 
     void GetMousePos()
@@ -18,8 +24,20 @@ public class TrineProtoPC : MonoBehaviour
         mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
     }
 
+    void GetRaycast()
+    {
+        RaycastHit hit;
+      
+        if(Physics.Raycast(transform.position, mousePos, out hit, rayLength, groundLayer))
+        {
+            raycastHit = hit.point;
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
+
+        Gizmos.DrawRay(Camera.main.transform.position, new Vector3(mousePos.x, mousePos.y, rayLength));
     }
 }
