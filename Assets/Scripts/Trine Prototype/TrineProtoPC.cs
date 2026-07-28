e1f559afa71db32b0dccb3c8152e9e2fd18b4d96
Am1d1v <dimadiman1adima@gmail.com>
2026-07-28 14:45:48 +0300

    Trine Prototype

--- Assets/Scripts/Trine Prototype/TrineProtoPC.cs
+++ Assets/Scripts/Trine Prototype/TrineProtoPC.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class TrineProtoPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] Vector2 mousePos;
+
+    private void Update()
+    {
+        GetMousePos();
+    }
+
+    void GetMousePos()
+    {
+        mousePos = Input.mousePosition;
+    }
 }
