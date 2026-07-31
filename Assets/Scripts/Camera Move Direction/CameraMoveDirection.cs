d7f2270cbffd0f82f84e54191528abfd6a6a8f5b
Am1d1v <dimadiman1adima@gmail.com>
2026-07-31 14:40:38 +0300

    Camera Move Direction

--- Assets/Scripts/Camera Move Direction/CameraMoveDirection.cs
+++ Assets/Scripts/Camera Move Direction/CameraMoveDirection.cs
@@ -28,6 +28,6 @@ public class CameraMoveDirection : MonoBehaviour
 
     void Rotate()
     {
-        
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z) + new Vector3(0f, cameraInput.x, 0f) * moveSpeed * Time.deltaTime);
     }
 }
