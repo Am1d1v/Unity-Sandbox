92e50573ea981a6d482e1edf764918fd36f2711b
Am1d1v <dimadiman1adima@gmail.com>
2026-08-01 00:47:32 +0300

    Parkour Base

--- Assets/Scripts/Parkour Test/ParkourPC.cs
+++ Assets/Scripts/Parkour Test/ParkourPC.cs
@@ -4,12 +4,20 @@ public class ParkourPC : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float checkRadius;
+    [SerializeField] float hangLenght;
     [SerializeField] Vector3 checkPoint;
+    [SerializeField] LayerMask hangLayer;
+
+
+    private void Update()
+    {
+        
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
 
-        Gizmos.DrawWireSphere(transform.position + checkPoint, checkRadius);
+        Gizmos.DrawWireSphere((transform.position + checkPoint) * hangLenght, checkRadius);
     }
 }
