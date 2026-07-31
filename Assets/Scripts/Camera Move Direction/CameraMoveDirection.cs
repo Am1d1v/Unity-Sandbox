d13576c0a7bb7b9f8e4dedf61ad414125504b237
Am1d1v <dimadiman1adima@gmail.com>
2026-07-31 14:19:49 +0300

    Camera Move Direction

--- Assets/Scripts/Camera Move Direction/CameraMoveDirection.cs
+++ Assets/Scripts/Camera Move Direction/CameraMoveDirection.cs
@@ -4,19 +4,30 @@ public class CameraMoveDirection : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] Vector2 cameraInput;
+    [SerializeField] Vector3 moveDirection;
+    [SerializeField] float moveSpeed;
 
     [Header("Elements")]
     [SerializeField] Camera activeCamera;
 
     private void Update()
     {
-        GetCameraInput();
+        //GetCameraInput();
+
+        Move();
     }
 
-    void GetCameraInput()
+    Vector3 GetCameraInput()
     {
         cameraInput.x = Input.GetAxis("Mouse X");
         cameraInput.y = Input.GetAxis("Mouse Y");
         cameraInput.Normalize();
+
+        return activeCamera.transform.forward * cameraInput;
+    }
+
+    void Move()
+    {
+        transform.position += GetCameraInput() * moveSpeed;
     }
 }
