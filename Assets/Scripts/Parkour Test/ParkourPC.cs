7ac959f2ce61820a021f6a6c57313f1a3cf74034
Am1d1v <dimadiman1adima@gmail.com>
2026-08-01 00:46:19 +0300

    Parkour Base

--- Assets/Scripts/Parkour Test/ParkourPC.cs
+++ Assets/Scripts/Parkour Test/ParkourPC.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class ParkourPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float checkRadius;
+    [SerializeField] Vector3 checkPoint;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawWireSphere(transform.position + checkPoint, checkRadius);
+    }
 }
