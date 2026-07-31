2419cc00c61c1790630b014c8c1937c69772fde8
Am1d1v <dimadiman1adima@gmail.com>
2026-08-01 01:08:16 +0300

    Parkour Base

--- Assets/Scripts/Parkour Test/ParkourPC.cs
+++ Assets/Scripts/Parkour Test/ParkourPC.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ParkourPC : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float checkRadius;
     [SerializeField] float hangLenght;
+    [SerializeField] float hangSpeed;
     [SerializeField] bool canHang;
+    [SerializeField] bool isHanged;
     [SerializeField] Vector3 checkPoint;
     [SerializeField] LayerMask hangLayer;
     [SerializeField] Transform hangObject;
@@ -14,13 +17,35 @@ public class ParkourPC : MonoBehaviour
     private void Update()
     {
         CheckToHang();
+
+        Hang();
     }
 
     void CheckToHang()
     {
         checkPoint = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * hangLenght;
 
-        canHang = Physics.CheckSphere(transform.position + checkPoint * hangLenght, checkRadius, hangLayer);
+        RaycastHit hit;
+
+        //canHang = Physics.CheckSphere(transform.position + checkPoint * hangLenght, checkRadius, hangLayer);
+
+        if(Physics.Raycast(transform.position, transform.position + checkPoint * hangLenght, out hit, hangLayer))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    void Hang()
+    {
+        if (canHang)
+        {
+            //transform.SetParent(hangObject);
+
+            if(transform.position != hangObject.position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, hangObject.position, hangSpeed * Time.deltaTime);
+            }
+        }
     }
 
     private void OnDrawGizmos()
