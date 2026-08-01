5ce0d224a3be8e25a914e680875f9403e5ecff97
Am1d1v <dimadiman1adima@gmail.com>
2026-08-01 21:13:04 +0300

    Camera Move Direction

--- Assets/Scripts/Camera Move Direction/CMDCinemacihinePC.cs
+++ Assets/Scripts/Camera Move Direction/CMDCinemacihinePC.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CMDCinemacihinePC : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 mouseInput;
 
     [Header("Elements")]
     [SerializeField] Camera mainCamera;
 
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference mouseDeltaAction;
+
     private void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        GetMouseInputDelta();
+    }
+
+    void GetMouseInputDelta()
+    {
+        mouseInput = mouseDeltaAction.action.ReadValue<Vector2>();
     }
 }
