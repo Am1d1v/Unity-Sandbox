6576972edb9a59b349929547d5de45fcda6bab1a
Am1d1v <dimadiman1adima@gmail.com>
2026-08-01 21:08:40 +0300

    Camera Move Direction

--- Assets/Scripts/Camera Move Direction/CMDCinemacihinePC.cs
+++ Assets/Scripts/Camera Move Direction/CMDCinemacihinePC.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class CMDCinemacihinePC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+
+    [Header("Elements")]
+    [SerializeField] Camera mainCamera;
+
+    private void Start()
+    {
+        
+    }
 }
