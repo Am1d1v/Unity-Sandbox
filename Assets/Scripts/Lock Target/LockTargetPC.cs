ef5da6cc25f910fdc475f8f2841705392459ec5d
Am1d1v <dimadiman1adima@gmail.com>
2026-08-03 21:09:11 +0300

    Target Lock

--- Assets/Scripts/Lock Target/LockTargetPC.cs
+++ Assets/Scripts/Lock Target/LockTargetPC.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class LockTargetPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 targetPosition;
+    [SerializeField] Vector3 moveInput;
+
+    private void Update()
+    {
+        LookAtTheTarget();
+    }
+
+    void LookAtTheTarget()
+    {
+        if (target == null) return;
+
+        targetPosition = target.position - transform.position;
+        targetPosition.y = 0f;
+
+        transform.rotation = Quaternion.LookRotation(targetPosition, transform.up);
+    }
 }
