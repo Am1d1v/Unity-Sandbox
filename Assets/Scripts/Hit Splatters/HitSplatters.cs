414850c0623bc72127cbad63edf0911f704517b1
Am1d1v <dimadiman1adima@gmail.com>
2026-08-05 20:24:32 +0300

    Hit Splatters

--- Assets/Scripts/Hit Splatters/HitSplatters.cs
+++ Assets/Scripts/Hit Splatters/HitSplatters.cs
@@ -6,19 +6,25 @@ public class HitSplatters : MonoBehaviour
     [Header("Elements")]
     [SerializeField] ParticleSystem bloodVFX;
     [SerializeField] List<ParticleCollisionEvent> bloodCollisionsEvent = new List<ParticleCollisionEvent>();
+    [SerializeField] List<Vector3> bloodPoints = new List<Vector3>();
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            bloodPoints.Clear();
+
             bloodVFX.Play();
         }
     }
 
     private void OnParticleCollision(GameObject other)
-    {
-        int splattersCount = bloodVFX.GetSafeCollisionEventSize();
+    {       
+        int splattersEventsCount = bloodVFX.GetCollisionEvents(other, bloodCollisionsEvent);
 
-        Debug.Log(splattersCount);
+        for(int i = 0; i < splattersEventsCount; i++)
+        {
+            bloodPoints.Add(bloodCollisionsEvent[i].intersection);
+        }
     }   
 }
