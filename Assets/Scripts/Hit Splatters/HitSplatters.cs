200f85610c6e863190f0041d056b48581ac4959e
Am1d1v <dimadiman1adima@gmail.com>
2026-08-05 19:53:41 +0300

    Hit Splatters

--- Assets/Scripts/Hit Splatters/HitSplatters.cs
+++ Assets/Scripts/Hit Splatters/HitSplatters.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class HitSplatters : MonoBehaviour
 {
-    
+    [Header("Elements")]
+    [SerializeField] ParticleSystem bloodVFX;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            bloodVFX.Play();
+        }
+    }
 }
