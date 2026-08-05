13f84e905e05ec1bd3fed912e394e923d7d3168a
Am1d1v <dimadiman1adima@gmail.com>
2026-08-05 16:22:33 +0300

    Dynamic Grid Set up

--- Assets/Scripts/Dynamic Grid Set Up/DGSUGridManager.cs
+++ Assets/Scripts/Dynamic Grid Set Up/DGSUGridManager.cs
@@ -6,4 +6,22 @@ public class DGSUGridManager : MonoBehaviour
     [SerializeField] int width;
     [SerializeField] int length;
     [SerializeField] GameObject gridPrefab;
+
+    private void Start()
+    {
+        SetGrid();
+    }
+
+    void SetGrid()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < length; j++)
+            {
+                GameObject grid = Instantiate(gridPrefab, gridPrefab.transform.position, Quaternion.identity, transform);
+
+                grid.transform.localPosition = new Vector3(i, 0f, j);
+            }
+        }
+    }
 }
