4586a5ee2d935f91671d8535fe3380fc2654cc21
Am1d1v <dimadiman1adima@gmail.com>
2026-08-09 14:21:11 +0300

    Loader Cart

--- Assets/Scripts/Loader Cart/LCItem.cs
+++ Assets/Scripts/Loader Cart/LCItem.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class LCItem : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float xSizeBordersmin;
+    [SerializeField] float xSizeBordersmax;
+    [SerializeField] float ySizeBordersmin;
+    [SerializeField] float ySizeBordersmax;
+    [SerializeField] float zSizeBordersmin;
+    [SerializeField] float zSizeBordersmax;
+
+    private void Start()
+    {
+        ConfigureItem();
+    }
+
+    void ConfigureItem()
+    {
+        float selectedXSize = Random.Range(xSizeBordersmin, xSizeBordersmax);
+        float selectedYSize = Random.Range(ySizeBordersmin, ySizeBordersmax);
+        float selectedZSize = Random.Range(zSizeBordersmin, zSizeBordersmax);
+
+        transform.localScale = new Vector3(selectedXSize, selectedYSize, selectedZSize);
+    }
 }
