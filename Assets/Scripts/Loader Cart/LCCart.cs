fab649e52ec8c4d6c982d21086c7ff618bea888c
Am1d1v <dimadiman1adima@gmail.com>
2026-08-09 12:43:58 +0300

    Loader Cart

--- Assets/Scripts/Loader Cart/LCCart.cs
+++ Assets/Scripts/Loader Cart/LCCart.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LCCart : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 moveDirection;
+
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference MoveInput;
+
+    private void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        moveDirection.x = MoveInput.action.ReadValue<Vector2>().x;
+    }
 }
