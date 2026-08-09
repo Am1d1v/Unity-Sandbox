02260a59988c5b51a2507b278bd86778bcbcde9e
Am1d1v <dimadiman1adima@gmail.com>
2026-08-09 14:25:14 +0300

    Loader Cart

--- Assets/Scripts/Loader Cart/LCItem.cs
+++ Assets/Scripts/Loader Cart/LCItem.cs
@@ -9,6 +9,7 @@ public class LCItem : MonoBehaviour
     [SerializeField] float ySizeBordersmax;
     [SerializeField] float zSizeBordersmin;
     [SerializeField] float zSizeBordersmax;
+    [field: SerializeField] public float totalValue { get; private set; }
 
     private void Start()
     {
@@ -20,6 +21,8 @@ public class LCItem : MonoBehaviour
         float selectedXSize = Random.Range(xSizeBordersmin, xSizeBordersmax);
         float selectedYSize = Random.Range(ySizeBordersmin, ySizeBordersmax);
         float selectedZSize = Random.Range(zSizeBordersmin, zSizeBordersmax);
+
+        totalValue = Mathf.Round(selectedXSize + selectedYSize + selectedZSize);
 
         transform.localScale = new Vector3(selectedXSize, selectedYSize, selectedZSize);
     }
