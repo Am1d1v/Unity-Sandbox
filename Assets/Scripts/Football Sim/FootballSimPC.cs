c837a80db372fe1ef356adc3b1e4e805027069c4
Am1d1v <dimadiman1adima@gmail.com>
2026-08-11 14:22:53 +0300

    Football Sim

--- Assets/Scripts/Football Sim/FootballSimPC.cs
+++ Assets/Scripts/Football Sim/FootballSimPC.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FootballSimPC : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float moveSpeed;
     [SerializeField] float rotationSpeed;
+
+    [Header("Elements")]
+    [SerializeField] CharacterController characterController;
+
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference moveInput;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        characterController.Move(transform.forward * moveInput.action.ReadValue<Vector2>().y * moveSpeed * Time.deltaTime);
+    }
 }
