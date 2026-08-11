853e5010f10f8d180b4015b46af662fbda54eeb5
Am1d1v <dimadiman1adima@gmail.com>
2026-08-11 14:18:58 +0300

    Football Sim

--- Assets/Scripts/Football Sim/FootballSimPC.cs
+++ Assets/Scripts/Football Sim/FootballSimPC.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class FootballSimPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
 }
