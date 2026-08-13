1756af96e542a7b6c991e911f08a29287c2d85bf
Am1d1v <dimadiman1adima@gmail.com>
2026-08-13 15:05:23 +0300

    Grid set up

--- Assets/Scripts/Grid Set up/GridSetUpManager.cs
+++ Assets/Scripts/Grid Set up/GridSetUpManager.cs
@@ -2,5 +2,27 @@ using UnityEngine;
 
 public class GridSetUpManager : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] int width;
+    [SerializeField] int length;
+    [SerializeField] Transform gridsHolder;
+    [SerializeField] GameObject gridPrefab;
+
+    private void Start()
+    {
+        SetGrid();
+    }
+
+    void SetGrid()
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < length; y++)
+            {
+                GameObject grid = Instantiate(gridPrefab, transform.position, Quaternion.identity, gridsHolder);
+
+                grid.transform.localPosition = new Vector3(x, 0f, y);
+            }
+        }
+    }
 }
