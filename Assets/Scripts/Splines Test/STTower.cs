f8ce5fbf506cf8ca1c9bd13b397507fce005f995
Am1d1v <dimadiman1adima@gmail.com>
2026-08-15 17:36:15 +0300

    Splines Test

--- Assets/Scripts/Splines Test/STTower.cs
+++ Assets/Scripts/Splines Test/STTower.cs
@@ -3,6 +3,9 @@ using UnityEngine.Splines;
 
 public class STTower : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] Transform playerPosition;
+
     [Header("Elements")]
     [SerializeField] SplineContainer splineContainer;
 
