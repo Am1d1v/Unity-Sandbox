b21710ede3492661875c6692efb53669e5096619
Am1d1v <dimadiman1adima@gmail.com>
2026-08-15 15:34:51 +0300

    Character controller

--- Assets/Scripts/Character Controller/CCCharacter.cs
+++ Assets/Scripts/Character Controller/CCCharacter.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class CCCharacter : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] Vector3 totalMovement;
+
+    [Header("Elements")]
+    [SerializeField] CharacterController controller;
+
+    private void Update()
+    {
+        CalculateYVelocity();
+
+        Move();
+    }
+
+    void CalculateYVelocity()
+    {
+        Vector3 movement = new Vector3(0f, Physics.gravity.y * Time.deltaTime, 0f);
+
+        if (controller.isGrounded)
+        {
+            totalMovement = movement;
+        }
+        else
+        {
+            totalMovement += movement;
+        }
+    }
+
+    void Move()
+    {
+        controller.Move(totalMovement);
+    }
 }
