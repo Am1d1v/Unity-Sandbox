56f2022c975b2d28db3adc7ba4651aabdb2b4a64
Am1d1v <dimadiman1adima@gmail.com>
2026-08-15 17:29:13 +0300

    Splines Test

--- Assets/Scripts/Splines Test/STPC.cs
+++ Assets/Scripts/Splines Test/STPC.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class STPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float splineMovementDuration;
+    [SerializeField] float splineProgress;
+
+    [Header("Elements")]
+    [SerializeField] SplineContainer splineContainer;
+
+    private void Update()
+    {
+        SplineMove();
+    }
+
+    void SplineMove()
+    {
+        splineProgress += Time.deltaTime * moveSpeed / splineMovementDuration;
+
+        transform.position = splineContainer.EvaluatePosition(splineProgress);
+
+        if(splineProgress >= 1f)
+        {
+            Debug.Log("Exit spline movement state");
+        }
+    }
 }
