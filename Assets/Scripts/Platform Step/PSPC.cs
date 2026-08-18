52c79a6c2b97f17686b2ae11a785d9e4779945b0
Am1d1v <dimadiman1adima@gmail.com>
2026-08-19 00:41:58 +0300

    Platform Step

--- Assets/Scripts/Platform Step/PSPC.cs
+++ Assets/Scripts/Platform Step/PSPC.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class PSPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float detectionRange;
+    [SerializeField] Vector3 detectionOffset;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+
+        Gizmos.DrawWireCube(transform.position + detectionOffset, Vector3.one * detectionRange);
+    }
 }
