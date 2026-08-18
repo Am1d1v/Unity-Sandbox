2be4e0f15cc2bebb99b3447deb2ce96c330c6878
Am1d1v <dimadiman1adima@gmail.com>
2026-08-18 14:47:57 +0300

    Character Rotation Control

--- Assets/Scripts/Character Rotation/CRPC.cs
+++ Assets/Scripts/Character Rotation/CRPC.cs
@@ -13,7 +13,8 @@ public class CRPC : MonoBehaviour
     private void Update()
     {
         //RotateMethod();
-        RotateMouse();
+        //RotateMouse();
+        RotateTransform();
     }
 
     void RotateMethod()
@@ -28,5 +29,14 @@ public class CRPC : MonoBehaviour
         rotationInput.x = Input.GetAxisRaw("Mouse X");
 
         transform.Rotate(new Vector2(0f, rotationInput.x) * rotationSpeed * Time.deltaTime);
+    }
+
+    void RotateTransform()
+    {
+        rotationInput.x = RotationInputAction.action.ReadValue<Vector2>().x;
+
+        if (rotationInput.x == 0) return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(rotationInput.x * transform.right * Time.deltaTime);
     }
 }
