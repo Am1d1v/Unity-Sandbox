2a162e8d543294dd25e7bc3b4b30a0713b400616
Am1d1v <dimadiman1adima@gmail.com>
2026-08-18 14:29:21 +0300

    Character Rotation Control

--- Assets/Scripts/Character Rotation/CRPC.cs
+++ Assets/Scripts/Character Rotation/CRPC.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CRPC : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [SerializeField] float rotationSpeed;
+    [SerializeField] Vector2 rotationInput;
+
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference RotationInputAction;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        rotationInput.x = RotationInputAction.action.ReadValue<Vector2>().x;
+
+
+    }
 }
