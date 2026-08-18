e024bc69a009b9c0fa3fa832d441663529bd83d8
Am1d1v <dimadiman1adima@gmail.com>
2026-08-19 00:46:04 +0300

    Platform Step

--- Assets/Scripts/Platform Step/PSPC.cs
+++ Assets/Scripts/Platform Step/PSPC.cs
@@ -5,6 +5,17 @@ public class PSPC : MonoBehaviour
     [Header("Settings")]
     [SerializeField] float detectionRange;
     [SerializeField] Vector3 detectionOffset;
+    [SerializeField] GameObject platformPrefab;
+
+    private void Start()
+    {
+        CreatePlatform();
+    }
+
+    void CreatePlatform()
+    {
+        Instantiate(platformPrefab, transform.position + detectionOffset, Quaternion.identity);
+    }
 
     private void OnDrawGizmos()
     {
