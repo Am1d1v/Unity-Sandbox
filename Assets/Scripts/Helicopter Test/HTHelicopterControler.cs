614b50187b4a2a3f56b4d17c23a23d623e0fb384
Am1d1v <dimadiman1adima@gmail.com>
2026-08-19 21:38:58 +0300

    Helicopter Test Controller

--- Assets/Scripts/Helicopter Test/HTHelicopterControler.cs
+++ Assets/Scripts/Helicopter Test/HTHelicopterControler.cs
@@ -1,10 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HTHelicopterControler : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] float moveSpeed;
+    [SerializeField] Vector3 yImpact;
+    [SerializeField] Vector3 totalImpact;
 
     [Header("Elements")]
     [SerializeField] CharacterController characterController;
+
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference MoveInput;
+
+    private void Update()
+    {
+        CalculateYVelocity();
+
+        Move();
+    }
+
+    void CalculateYVelocity()
+    {
+        if (characterController.isGrounded)
+        {
+            yImpact.y = Physics.gravity.y * Time.deltaTime;
+        }
+        else
+        {
+            yImpact.y += Physics.gravity.y * Time.deltaTime;
+        }
+    }
+
+    void Move()
+    {
+        characterController.Move((totalImpact + yImpact) * Time.deltaTime);
+    }
 }
