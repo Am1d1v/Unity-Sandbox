66788a204a59147edcbf07c2c396a328836f0389
Am1d1v <dimadiman1adima@gmail.com>
2026-08-21 16:01:50 +0300

    Particle Collisions

--- Assets/Scripts/Particle Collisions Test/PCCollisions.cs
+++ Assets/Scripts/Particle Collisions Test/PCCollisions.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class PCCollisions : MonoBehaviour
 {
-    
+    [Header("Elements")]
+    [SerializeField] ParticleSystem collisionParticles;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            collisionParticles.Play();
+        }
+    }
 }
